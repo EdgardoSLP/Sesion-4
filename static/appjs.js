aa910e750e3d74ae5f788d24a628b4e7bf4d868a
EdgardoSLP <00342717@red.unid.mx>
2020-05-29 23:43:16 -0500

    Función Insertar-Filtrar-Eliminar

--- static/appjs.js
+++ static/appjs.js
@@ -1,6 +1,6 @@
 
 // Your web app's Firebase configuration
-var firebaseConfig = {
+/*var firebaseConfig = {
     apiKey: "AIzaSyAOjJh5tLTxrrSMMcmzg10hQLHAPJInAjk",
     authDomain: "chatapp-2a11f.firebaseapp.com",
     databaseURL: "https://chatapp-2a11f.firebaseio.com",
@@ -9,7 +9,20 @@ var firebaseConfig = {
     messagingSenderId: "157134947168",
     appId: "1:157134947168:web:3304e879a09e45931a5405",
     measurementId: "G-PCW5869LNL"
-};
+};*/
+/*Se habilito una nueva cadena de conexion a una nueva Base de Datos e Firebase
+debido a que el servidor llego al tope en solicitud de consultas*/
+var firebaseConfig = {
+    apiKey: "AIzaSyBQxF15lOBk4qejuvnw9h-4yLe48Xe20NE",
+    authDomain: "chatapp-98d00.firebaseapp.com",
+    databaseURL: "https://chatapp-98d00.firebaseio.com",
+    projectId: "chatapp-98d00",
+    storageBucket: "chatapp-98d00.appspot.com",
+    messagingSenderId: "1017162347060",
+    appId: "1:1017162347060:web:472317d8d5adac4e491061",
+    measurementId: "G-0GYGF64HTC"
+  };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
@@ -26,17 +39,20 @@ firebase.analytics();
         let divtd = document.createElement('div');
         let divbr = document.createElement('div');
         let divcon = document.createElement('div');
+        //let divtr = document.createComment('div');
         let divs = document.createElement('div');
 
 
         div.setAttribute('class', "row");
         div.setAttribute('data-id', doc.id); //guardamos el id en el html
+        div.setAttribute('ondblclick',"eliminar(this)");//Se aplica la funcion DobleClick del Mouse para eliminar el mensaje
         divc.setAttribute('class', "content");
         divtp.setAttribute('class',"top-row");
         divcn.setAttribute('class',"chatname");
         divtd.setAttribute('class',"timedate");
         divbr.setAttribute('class',"bottom-row");
         divcon.setAttribute('class',"contactname");
+        //divtr.setAttribute('class',"trash");
         divs.setAttribute('class',"status");
 
         div.appendChild(divc);
@@ -45,6 +61,7 @@ firebase.analytics();
         divtp.appendChild(divcn);
         divtp.appendChild(divtd);
         divbr.appendChild(divcon);
+        //divbr.appendChild(divtr);
         divbr.appendChild(divs);
             
         //to.textContent = doc.data().to;
@@ -57,18 +74,25 @@ firebase.analytics();
         document.querySelector("body").appendChild(div);
     }
 
+var minombre = "Edgardo";
+
+/*Se crea la función Insert para enviar mensaje a un contacto en especifico  */
 function insert(){
     var referencia = db.collection('chat');
     //alert("si entro");
-        const fr = "Edgardo";
+        const fr = minombre;
         var para = "";
         var mensaje = "";
         var f = new Date();
+        //var filtro = "";
         //document.write(f.getDate() + "/" + (f.getMonth() +1) + "/" + f.getFullYear());
 
+        /*Se toman los valores que el usuario ingreo en los campos y se almacenan en una variable*/
         para = document.getElementById("to").value;
         mensaje = document.getElementById("message").value;
+        //filtro = document.getElementById("to").value;
         
+        //Se envian los datos a la base de datos
         referencia.add({
             from:fr,
             message:mensaje,
@@ -76,7 +100,6 @@ function insert(){
             to:para,
         }) .then(function(docRef) {
             console.log("Document successfully written!");
-            //document.querySelector('.rowc').appendChild(divrow3);
             db.collection("chat").doc(docRef.id).get().then(function(doc){
                 //console.log(doc.id);
                 renderChat(doc)});
@@ -84,7 +107,8 @@ function insert(){
         .catch(function(error) {
             console.error("Error writing document: ", error);
         });
-    
+
+        //filtrar(filtro);    
         /*var nuevo = new Object();
         nuevo.from = fr;
         nuevo.message = mensaje;
@@ -106,15 +130,52 @@ function insert(){
         nuevo.to = para;*/
     
         //renderChat(nuevo);
-
-
 }
 
-db.collection('chat').orderBy('timestamp').get().then((snapshots)=>{ //function es lo mismo =>
+/*db.collection('chat').orderBy('timestamp').get().then((snapshots)=>{ //function es lo mismo =>
     snapshots.docs.forEach(doc=>{
         renderChat(doc);
     });
-});
+});*/
+
+//Se crea la función para filtrar las conversaciones por un usuario en especifico
+function filtrar(){
+    document.querySelector(".principal").innerHTML = "";
+    user = document.getElementById("to").value;
+    const fromto = db.collection('chat').where('to', '==', user );
+    const tofrom = db.collection('chat').where('from', '==', user);
+    //Se almacenan en variables, los mensajes enviados por el usuario, asi como los mensajes recibidos del usuario
+
+    fromto.get()
+    .then((snapshots)=>{
+        snapshots.docs.forEach(doc =>{
+            renderChat(doc);
+            console.log(doc.id, " => ", doc.data());
+        });
+
+    });
+    tofrom.get()
+    .then((snapshots)=>{
+        snapshots.docs.forEach(doc =>{
+            renderChat(doc);
+            console.log(doc.id, " => ", doc.data());
+        });
+    });
+    //Se obtienen la información de los mensajes de usuario (enviados y recibidos) y se envian a la sección de chat
+
+};
+
+/*Se crea la función eliminar, para eliminar algun mensaje en especifico, de da dobleclick en el mensaje y manda llamar 
+a la función eliminar (No funciona correctamente, marca error al mandar llamar a la función)*/
+function eliminar(){
+    console.log("La Función Entro Correctamente");
+    db.collection("chat").doc(id).delete().then(function() {
+        console.log("Document successfully deleted!");
+    }).catch(function(error) {
+        console.error("Error removing document: ", error);
+    });
+
+};
 
 
 /* Utilice estas instrucciones de JavaScript para dar el efecto 
@@ -153,11 +214,7 @@ function duplicate(){
        document.querySelector('.row-'+i).innerHTML += str_html;
        //concatenamos el "row-i" y lo concatenamos con un valor igual almacenado en la consola
       }
-     
-   
     }
-   
-   
    }
    //Esta función activa DIV estados "como seleccionado"
    // y elimina la selección del resto
@@ -204,4 +261,17 @@ function duplicate(){
        insert();
    }
 
+   document.querySelectorAll('.contactname').onclick=function(){
+        eliminar();
+    }
+
+    document.querySelectorAll('.to').onclick=function(){
+        filtrar();
+    }
+
 });
+
+
+
+
+
